de0faa94ac4b478c0f8cd6411ba0ab2eaf2eb62a
Lifan Sun <lifansun1412@gmail.com>
2024-08-15 16:49:01 +0800

    fix indent issue

---
+++ tests/test_engine.py
@@ -0,0 +1,222 @@
+import torch
+from tinygrad.engine import Value
+
+def test_value_operations():
+    # Basic tests
+    a = Value(2.0)
+    b = Value(3.0)
+    c = a + b
+    assert c.data == 5.0
+
+    a = Value(2.0)
+    b = Value(3.0)
+    c = a * b
+    assert c.data == 6.0
+
+    a = Value(2.0)
+    b = a ** 3
+    assert b.data == 8.0
+
+    a = Value(-1.0)
+    b = Value.relu(a)
+    assert b.data == 0.0
+    a = Value(1.0)
+    b = Value.relu(a)
+    assert b.data == 1.0
+
+    a = Value(2.0)
+    b = -a
+    assert b.data == -2.0
+
+    a = Value(5.0)
+    b = Value(3.0)
+    c = a - b
+    assert c.data == 2.0
+
+    a = Value(6.0)
+    b = Value(3.0)
+    c = a / b
+    assert c.data == 2.0
+
+    a = Value(2.0)
+    b = 3 + a
+    assert b.data == 5.0
+
+    a = Value(2.0)
+    b = 3 - a
+    assert b.data == 1.0
+
+    a = Value(2.0)
+    b = 3 * a
+    assert b.data == 6.0
+
+    a = Value(2.0)
+    b = 6 / a
+    assert b.data == 3.0
+
+def test_value_backward():
+    # Backward tests
+    a = Value(2.0)
+    b = Value(3.0)
+    c = a + b
+    c.backward()
+    assert a.grad == 1.0
+    assert b.grad == 1.0
+
+    a = Value(2.0)
+    b = Value(3.0)
+    c = a * b
+    c.backward()
+    assert a.grad == 3.0
+    assert b.grad == 2.0
+
+def test_value_with_torch():
+    # Torch comparison tests
+    a = torch.tensor(2.0, requires_grad=True)
+    b = torch.tensor(3.0, requires_grad=True)
+    c = a + b
+    c.backward()
+    a_val = Value(2.0)
+    b_val = Value(3.0)
+    c_val = a_val + b_val
+    c_val.backward()
+    assert torch.isclose(torch.tensor(c_val.data), c)
+    assert torch.isclose(torch.tensor(a_val.grad), a.grad)
+    assert torch.isclose(torch.tensor(b_val.grad), b.grad)
+
+    a = torch.tensor(2.0, requires_grad=True)
+    b = torch.tensor(3.0, requires_grad=True)
+    c = a * b
+    c.backward()
+    a_val = Value(2.0)
+    b_val = Value(3.0)
+    c_val = a_val * b_val
+    c_val.backward()
+    assert torch.isclose(torch.tensor(c_val.data), c)
+    assert torch.isclose(torch.tensor(a_val.grad), a.grad)
+    assert torch.isclose(torch.tensor(b_val.grad), b.grad)
+
+    a = torch.tensor(2.0, requires_grad=True)
+    b = a ** 3
+    b.backward()
+    a_val = Value(2.0)
+    b_val = a_val ** 3
+    b_val.backward()
+    assert torch.isclose(torch.tensor(b_val.data), b)
+    assert torch.isclose(torch.tensor(a_val.grad), a.grad)
+
+    a = torch.tensor(-1.0, requires_grad=True)
+    b = torch.relu(a)
+    b.backward()
+    a_val = Value(-1.0)
+    b_val = Value.relu(a_val)
+    b_val.backward()
+    assert torch.isclose(torch.tensor(b_val.data), b)
+    assert torch.isclose(torch.tensor(a_val.grad), a.grad)
+
+    a = torch.tensor(2.0, requires_grad=True)
+    b = -a
+    b.backward()
+    a_val = Value(2.0)
+    b_val = -a_val
+    b_val.backward()
+    assert torch.isclose(torch.tensor(b_val.data), b)
+    assert torch.isclose(torch.tensor(a_val.grad), a.grad)
+
+    a = torch.tensor(5.0, requires_grad=True)
+    b = torch.tensor(3.0, requires_grad=True)
+    c = a - b
+    c.backward()
+    a_val = Value(5.0)
+    b_val = Value(3.0)
+    c_val = a_val - b_val
+    c_val.backward()
+    assert torch.isclose(torch.tensor(c_val.data), c)
+    assert torch.isclose(torch.tensor(a_val.grad), a.grad)
+    assert torch.isclose(torch.tensor(b_val.grad), b.grad)
+
+    a = torch.tensor(6.0, requires_grad=True)
+    b = torch.tensor(3.0, requires_grad=True)
+    c = a / b
+    c.backward()
+    a_val = Value(6.0)
+    b_val = Value(3.0)
+    c_val = a_val / b_val
+    c_val.backward()
+    assert torch.isclose(torch.tensor(c_val.data), c)
+    assert torch.isclose(torch.tensor(a_val.grad), a.grad)
+    assert torch.isclose(torch.tensor(b_val.grad), b.grad)
+
+def test_value_with_torch_complex():
+    # Complex operation 1: (a + b) * c
+    a = torch.tensor(2.0, requires_grad=True)
+    b = torch.tensor(3.0, requires_grad=True)
+    c = torch.tensor(4.0, requires_grad=True)
+    d = (a + b) * c
+    d.backward()
+    a_val = Value(2.0)
+    b_val = Value(3.0)
+    c_val = Value(4.0)
+    d_val = (a_val + b_val) * c_val
+    d_val.backward()
+    assert torch.isclose(torch.tensor(d_val.data), d)
+    assert torch.isclose(torch.tensor(a_val.grad), a.grad)
+    assert torch.isclose(torch.tensor(b_val.grad), b.grad)
+    assert torch.isclose(torch.tensor(c_val.grad), c.grad)
+
+    # Complex operation 2: (a * b) + (c / d)
+    a = torch.tensor(2.0, requires_grad=True)
+    b = torch.tensor(3.0, requires_grad=True)
+    c = torch.tensor(4.0, requires_grad=True)
+    d = torch.tensor(5.0, requires_grad=True)
+    e = (a * b) + (c / d)
+    e.backward()
+    a_val = Value(2.0)
+    b_val = Value(3.0)
+    c_val = Value(4.0)
+    d_val = Value(5.0)
+    e_val = (a_val * b_val) + (c_val / d_val)
+    e_val.backward()
+    assert torch.isclose(torch.tensor(e_val.data), e)
+    assert torch.isclose(torch.tensor(a_val.grad), a.grad)
+    assert torch.isclose(torch.tensor(b_val.grad), b.grad)
+    assert torch.isclose(torch.tensor(c_val.grad), c.grad)
+    assert torch.isclose(torch.tensor(d_val.grad), d.grad)
+
+    # Complex operation 3: relu(a * b + c) - d
+    a = torch.tensor(2.0, requires_grad=True)
+    b = torch.tensor(3.0, requires_grad=True)
+    c = torch.tensor(4.0, requires_grad=True)
+    d = torch.tensor(5.0, requires_grad=True)
+    e = torch.relu(a * b + c) - d
+    e.backward()
+    a_val = Value(2.0)
+    b_val = Value(3.0)
+    c_val = Value(4.0)
+    d_val = Value(5.0)
+    e_val = Value.relu(a_val * b_val + c_val) - d_val
+    e_val.backward()
+    assert torch.isclose(torch.tensor(e_val.data), e)
+    assert torch.isclose(torch.tensor(a_val.grad), a.grad)
+    assert torch.isclose(torch.tensor(b_val.grad), b.grad)
+    assert torch.isclose(torch.tensor(c_val.grad), c.grad)
+    assert torch.isclose(torch.tensor(d_val.grad), d.grad)
+
+    # Complex operation 4: (a ** 2 + b ** 3) / (c - d)
+    a = torch.tensor(2.0, requires_grad=True)
+    b = torch.tensor(3.0, requires_grad=True)
+    c = torch.tensor(4.0, requires_grad=True)
+    d = torch.tensor(5.0, requires_grad=True)
+    e = (a ** 2 + b ** 3) / (c - d)
+    e.backward()
+    a_val = Value(2.0)
+    b_val = Value(3.0)
+    c_val = Value(4.0)
+    d_val = Value(5.0)
+    e_val = (a_val ** 2 + b_val ** 3) / (c_val - d_val)
+    e_val.backward()
+    assert torch.isclose(torch.tensor(e_val.data), e)
+    assert torch.isclose(torch.tensor(a_val.grad), a.grad)
+    assert torch.isclose(torch.tensor(b_val.grad), b.grad)
+    assert torch.isclose(torch.tensor(c_val.grad), c.grad)
+    assert torch.isclose(torch.tensor(d_val.grad), d.grad)
